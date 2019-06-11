dba78811656c3510647603b5b7037c96aaa635d9
lmartinezn <lmartinezn@edu.tecnocampus.cat>
2019-06-11 09:30:54 +0200

    Added message class

--- app/src/main/java/cat/tecnocampus/mobileapps/chat_test/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/chat_test/MainActivity.java
@@ -3,7 +3,11 @@ package cat.tecnocampus.mobileapps.chat_test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+
 public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mFirebaseDatabaseReference;
+    private FirebaseRecyclerAdapter<Message, MessageViewHolder> mFirebaseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
